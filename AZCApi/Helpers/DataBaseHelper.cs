178634046c226d40f46c35aa41d94cd6acfdd5c1
alexsey02 <aleksey.nadzir@mail.ru>
2021-12-16 17:27:15 +0300

    ывыаываа

---
+++ AZCApi/Helpers/DataBaseHelper.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AZCApi
+{
+    public class DataBaseHelper : DbContext
+    {
+        public DbSet<Station> Station { get; set; }
+        public DbSet<Date> Date {get; set; }
+
+        public DataBaseHelper(DbContextOptions<DataBaseHelper> options)
+            : base(options)
+        {
+
+        }
+
+        public DataBaseHelper()
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) =>
+            optionsBuilder.UseSqlServer("Data Source=FATHER\\FATHER;Initial Catalog=azc;Integrated Security=True");
+    }
+}
